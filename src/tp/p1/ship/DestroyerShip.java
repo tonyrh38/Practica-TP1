56805eec03f6a7d0f4fb3087a4a1dbad1de7a441
tonyrh38 <antrodrigh@gmail.com>
2019-09-25 13:05:05 +0200

    Llenar naves

--- src/tp/p1/ship/DestroyerShip.java
+++ src/tp/p1/ship/DestroyerShip.java
@@ -1,5 +1,56 @@
 package tp.p1.ship;
 
 public class DestroyerShip {
-
+	//Atributos
+			private int _x;
+			private int _y;
+			private int _vida;
+			private int _daño;
+			private int _puntos;
+			
+	//Metodos
+	
+	//Constructor
+		public DestroyerShip(int x, int y) {
+			this.setX(x);
+			this.setY(y);
+			this.setVida(1);
+			this.setDaño(1);
+			this.setPuntos(10);
+		}
+		
+	//Getters
+		public int getX() {
+			return _x;
+		}
+		public int getY() {
+			return _y;
+		}
+		public int getVida() {
+			return _vida;
+		}
+		public int getDaño() {
+			return _daño;
+		}
+		public int getPuntos() {
+			return _puntos;
+		}
+		
+	//Setters
+		public void setX(int x) {
+			this._x = x;
+		}
+		public void setY(int y) {
+			this._y = y;
+		}
+		public void setVida(int vida) {
+			this._vida = vida;
+		}
+		public void setDaño(int daño) {
+			this._daño = daño;
+		}
+		public void setPuntos(int puntos) {
+			this._puntos = puntos;
+		}
+		
 }
--- src/tp/p1/ship/UCMShip.java
+++ src/tp/p1/ship/UCMShip.java
@@ -1,5 +1,40 @@
 package tp.p1.ship;
 
 public class UCMShip {
-
+	
+	//Atributos
+		private int _x;
+		private int _y;
+		private int _daño;
+	
+	//Metodos
+	
+	//Constructor
+		public UCMShip() {
+			this.setX(7);
+			this.setY(4);
+			this.setDaño(1);
+		}
+	
+	//Getters 
+		public int getX() {
+			return this._x;
+		}
+		public int getY() {
+			return _y;
+		}
+		public int getDaño() {
+			return _daño;
+		}
+	
+	//Setters
+		public void setX(int x) {
+			this._x = x;
+		}
+		public void setY(int y) {
+			this._y = y;
+		}
+		public void setDaño(int daño) {
+			this._daño = daño;
+		}
 }
