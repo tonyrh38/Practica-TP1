8c1609e7a515badf34cdf77d4b2dfdc2f84012d9
tonyrh38 <antrodrigh@gmail.com>
2019-10-08 10:34:48 +0200

    Gotta go!

--- src/tp/p1/Main.java
+++ src/tp/p1/Main.java
@@ -1,10 +1,27 @@
 package tp.p1;
 
+import tp.p1.game.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	Game game;
+	if (args.length < 1 || args.length > 2) {
+	
+		Level level = Level.valueOf(args[0]);
+	
+		if (args.length == 2) {
+			int seed = Integer.parseInt(args[1]);
+			game = new Game(level, seed);
+		} else {
+			game = new Game(level);
+		}
+	
+		Controller controller = new Controller(game);
+		controller.run();
+	
+		System.exit(0);
 	}
 
 }
--- src/tp/p1/game/Game.java
+++ src/tp/p1/game/Game.java
@@ -1,5 +1,54 @@
 package tp.p1.game;
 
-public class Game {
+import java.util.*;
+import tp.p1.shipList.*;
 
+public class Game {
+	
+	private int cycleCounter;
+	private int puntuation;
+	private Level level;
+	private long seed;
+
+	
+	private RegularShipList regularShipList;
+	private DestroyerShipList destroyerShipList;
+	private BombList bombList;
+	
+	private Random rand;
+	
+	public Game(Level level) {
+		this(level, System.currentTimeMillis());
+	}
+	public Game(Level level, long seed) {
+		this(8, 4, level, seed);
+	}
+	
+	public Game(int width, int height, Level level) {	
+		this(width, height, level, System.currentTimeMillis());
+	}
+	
+	public Game(int width, int height, Level level, long seed) {
+		this.seed = seed;
+		this.level = level;
+		
+		this.reset(width, height);
+		
+		this.gamePrinter = new ReleasePrinter(this);
+	}
+	
+	public void reset() {
+		reset(this.getWidth(), this.getHeight());
+	}
+	
+	public void reset(int width, int height) {
+		this.cycleCount = 0;
+		
+		this.rand = new Random(this.seed);		
+		this.sunManager = new SunManager(this, this.rand, 50);
+		this.zombieManager = new ZombieManager(this.level, this.rand);
+		this.board = new Board(width, height, this.level.getMaxZombies());
+	}
+
+	
 }
